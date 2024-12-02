9ed4dd62b1bc76b8567891c1f32307f30ca15afc
Guilherme Gabriel Ziger Granville <161463539+guilherme-granville@users.noreply.github.com>
2024-12-02 20:28:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// script.js
-
 document.getElementById('hamburger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.getElementById('nav-menu').classList.toggle('active');
     document.body.classList.toggle('menu-open');  // Bloqueia o scroll
 });
 
-// Fechar o menu ao clicar em qualquer item de navegação
 document.querySelectorAll('#nav-menu a').forEach(item => {
     item.addEventListener('click', () => {
         document.getElementById('hamburger').classList.remove('active');
@@ -15,7 +12,6 @@ document.querySelectorAll('#nav-menu a').forEach(item => {
     });
 });
 
-// Fechar o menu ao clicar fora do menu
 document.addEventListener('click', function(event) {
     if (!document.getElementById('hamburger').contains(event.target) && !document.getElementById('nav-menu').contains(event.target)) {
         document.getElementById('hamburger').classList.remove('active');
