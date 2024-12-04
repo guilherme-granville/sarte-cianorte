3560203e14a7bd80b3ace792aaacc597f7785d9d
Guilherme Gabriel Ziger Granville <161463539+guilherme-granville@users.noreply.github.com>
2024-12-04 00:52:44 -0300

    Add files via upload

---
+++ inputs.js
@@ -0,0 +1,34 @@
+IMask(
+    document.getElementById('celular'), {
+        mask: '(00)00000-0000',
+        lazy: false,
+    }
+);
+
+IMask(
+    document.getElementById('telefone'), {
+        mask: '(00)00000-0000',
+        lazy: false,
+    }
+);
+
+IMask(
+    document.getElementById('cep'), {
+        mask: '00000-000',
+        lazy: false,
+    }
+);
+
+IMask(
+    document.getElementById('cpf'), {
+        mask: '000.000.000-00',
+        lazy: false,
+    }
+);
+
+IMask(    
+    document.getElementById('rg'), {
+        mask: '00.000.000-0',
+        lazy: false,
+    }    
+);
